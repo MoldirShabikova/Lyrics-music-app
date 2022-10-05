426ccfe0790498cec2f08b1220af4cf69da2156b
moldir <moldirshb@gmail.com>
2022-10-04 23:26:58 -0500

    added SongCard functionality and play/pause

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,9 +1,12 @@
+import { useDispatch, useSelector } from 'react-redux'
 import {Error, Loader, SongCard} from '../components'
 import {genres} from '../assets/constants'
 import {useGetTopChartsQuery} from '../redux/services/shazamCore'
 
 
 const Discover = () => {
+    const dispatch = useDispatch()
+    const {activeSong, isPlaying} = useSelector((state)=>state.player)
     const {data, isFetching, error} = useGetTopChartsQuery()
     const genreTitle = 'Pop'
 if(isFetching) return <Loader title="Loading songs..."/>
@@ -20,7 +23,7 @@ if(error) return <Error/>
           </div>
           <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
             {data?.map((song, i)=>(
-                <SongCard key={song.key} song={song} i={i}/>
+                <SongCard key={song.key} song={song} isPlaying={isPlaying} activeSong={activeSong} data={data} i={i} />
             ))}
             
           </div>
