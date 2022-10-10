f6b2561b9a2df4190aee24ebd8b12fde23e3821c
moldir <moldirshb@gmail.com>
2022-10-09 22:58:45 -0500

    search done

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -14,9 +14,11 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
         endpoints: (builder)=>({
             getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
             getTopCharts: builder.query({query: ()=> '/charts/world'}),
+            getSongsByGenre: builder.query({ query: (genre)=> `/charts/genre-world?genre_code=${genre}`}),
             getSongRelated: builder.query({query: ({songid})=>`/tracks/related?track_id=${songid}`}),
             getArtistDetails: builder.query({query: (artistId)=> `/artists/details?artist_id=${artistId}`}),
             getSongsByCountry: builder.query({query: (countryCode)=> `/charts/country?country_code=${countryCode}`}),
+            getSongsBySearch: builder.query({query: (searchTerm)=> `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`}),
         })
 
     })
@@ -24,8 +26,10 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
     export const {
         useGetTopChartsQuery,
         useGetSongDetailsQuery,
+        useGetSongsByGenreQuery,
         useGetSongRelatedQuery,
         useGetArtistDetailsQuery,
         useGetSongsByCountryQuery,
+        useGetSongsBySearchQuery,
 
     }= shazamCoreApi;
